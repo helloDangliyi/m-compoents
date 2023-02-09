2b59318830dd969d2f6c4cddeb6d85c490de4989
DDang-L <1968806729@qq.com>
2023-02-09 23:25:12 +0800

    制作菜单

---
+++ src/components/container/navSide/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-menu :collapse="collapse" default-active="2" class="el-menu-vertical-demo">
+    <el-menu-item index="1">
+      <el-icon-menu />
+      <template #title>首页</template>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <el-icon-menu />
+      <template #title>图标选择器</template>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <el-icon-menu />
+      <template #title>趋势标记</template>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script lang='ts' setup>
+let props = defineProps<{
+  collapse: boolean
+}>()
+
+</script>
+
+<style lang="scss" scoped>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,19 @@
 import { createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+import Container from '../components/container/src/index.vue'
 
 import Home from '../views/Home.vue';
 const routes: RouteRecordRaw[]= [
     {
-        path:'/',
-        component: Home
+        path: '/',
+        component: Container,
+        children: [
+            {
+                path:'/',
+                component: Home
+            }
+        ]
     }
+    
 ]
 
 const router = createRouter({
