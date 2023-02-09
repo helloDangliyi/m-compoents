ecd4eb4fec9096f45fcb77636742b3fbc62b6496
DDang-L <1968806729@qq.com>
2023-02-09 22:34:51 +0800

    添加icon图标"

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,22 @@ import App from './App.vue'
 import router from './router/index'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as Icons from '@element-plus/icons-vue'
+import { toLine } from './utils'
 
 const app = createApp(App)
+
 app.use(router).use(ElementPlus)
+
+// 全局注册图标，牺牲一点性能
+// 转换图标的命名为：el-icon-xxx
+// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+//     app.component(key, component)
+// }
+
+for( let i in Icons) {
+    app.component(`el-icon-${toLine(i)}`,(Icons as any)[i])
+    
+}
+
 app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div>
     首页
     <el-button>Default</el-button>
+    <el-icon-edit/>
   </div>
 </template>
 
@@ -9,3 +10,4 @@
 </script>
 
 <style lang="scss" scoped></style>
+<!-- 因为element-plus图标改版了，所有的图标都变成了组件，并且是用svg的形式来展示的 -->
