cb82bd630728087e3b70af8cf4178972caac081a
DDang-L <1968806729@qq.com>
2023-02-21 22:56:20 +0800

    趋势标记

--- src/components/trend/src/index.vue
+++ src/components/trend/src/index.vue
@@ -1,9 +1,98 @@
 <template>
-  <div>111</div>
+  <div class="trend">
+
+    <div class="text" :style="{color:textColor}">
+      <div v-if="slots.default">
+        <slot></slot>
+      </div>
+      <div v-else>{{ text }}</div>
+      
+    </div>
+    <div class="icon">
+      <!-- <el-icon-arrowup :style="{color: !reverseColor ? upIconColor:'#52c41a'}" v-if="type === 'up'"></el-icon-arrowup>
+      <el-icon-arrowdown :style="{color: !reverseColor ? downIconColor:'#f5222d'}" v-else></el-icon-arrowdown> -->
+
+      <component :is="`el-icon-${toLine(upIcon)}`" v-if="type === 'up'" :style="{color: !reverseColor ? upIconColor:'#52c41a'}"></component>
+      <component :is="`el-icon-${toLine(downIcon)}`" v-else :style="{color: !reverseColor ? downIconColor:'#f5222d'}"></component>
+    </div>
+
+  </div>
 </template>
 
 <script lang='ts' setup>
-import {} from "vue"
+import { useSlots,computed} from "vue"
+import { toLine } from "../../../utils";
+
+let props = defineProps({
+  // 标记当前趋势是上升(up)还是下降(down)
+  type:{
+    type: String,
+    default:'up'
+  },
+  // 上升趋势图标
+  upIcon:{
+    type: String,
+    default:'arrowup'
+  },
+  // 下降趋势图标
+  downIcon:{
+    type: String,
+    default:'arrowup'
+  },
+  // 趋势显示的文字
+  // 1. 父组件传递过来的数据
+  // 2.插槽
+  text :{
+    type: String,
+    default: '文字'
+  },
+  // 上升趋势的图标颜色
+  upIconColor:{
+    type: String,
+    default: '#f5222d'
+  },
+  downIconColor: {
+    type: String,
+    default:'#52c41a'
+  },
+  // 颜色反转只在默认的颜色下生效，如果使用了自定义的颜色，这个属性就不生效了
+  reverseColor:{
+    type: Boolean,
+    default: false
+  },
+  // 上升趋势的文字颜色
+  upTextColor:{
+    type: String,
+    default: '#000'
+  },
+  downTextColor: {
+    type: String,
+    default:'#000'
+  },
+})
+
+let slots =useSlots()
+console.log('slots: ', slots);
+
+let textColor = computed(()=> {
+  return props.type === 'up' ? props.upTextColor: props.downTextColor
+})
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.trend{
+  display: flex;
+  align-items: center;
+  .text{
+    font-size: 12px;
+    margin-right: 4px;
+  }
+
+  .icon {
+    .svg {
+      width: 0.8em;
+      height: 0.8em;
+    }
+  }
+}
+</style>
--- src/views/trend/index.vue
+++ src/views/trend/index.vue
@@ -1,9 +1,62 @@
 <template>
-  <m-trend></m-trend>
+  <!-- 基础使用：1. 父组件传递过来的数据 -->
+  <!-- 
+     <div class="flex">
+    <div>
+      <m-trend text="营业额" upIconColor="pink"></m-trend>
+    </div>
+    <div>
+      <m-trend text="销售额" type="'down'" downIconColor="purple"></m-trend>
+    </div>
+  </div>
+   -->
+ 
+
+  <br>
+
+  <!-- 2.插槽; -->
+  <!-- <div class="flex">
+    <div>
+      <m-trend text="营业额">营业额2</m-trend>
+    </div>
+    <div>
+      <m-trend text="销售额" type="down" ></m-trend>
+    </div>
+  </div> -->
+
+
+  <!-- 图标颜色反转 ：？？？他在说什么？-->
+  <div class="flex">
+    <div>
+      <m-trend text="营业额" reverseColor></m-trend>
+    </div>
+    <div>
+      <m-trend text="销售额" type="down" ></m-trend>
+    </div>
+  </div>
+
+  <!-- 文字颜色的改变,图标 -->
+  <!-- <div class="flex">
+    <div>
+      <m-trend text="营业额" upIcon="avatar"></m-trend>
+    </div>
+    <div>
+      <m-trend text="销售额" downTextColor="pink" type="down" ></m-trend>
+    </div>
+  </div> -->
+  
+  
 </template>
 
 <script lang='ts' setup>
 import {} from "vue"
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.flex{
+  display: flex;
+  div{
+    margin-right: 10px;
+  }
+}
+</style>
