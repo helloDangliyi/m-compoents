83e1b00981326f3009a9fbfb92ca777c4aba77d5
DDang-L <1968806729@qq.com>
2023-03-07 22:34:53 +0800

    时间选择器的封装

--- src/views/chooseTime/index.vue
+++ src/views/chooseTime/index.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
-    <m-choose-time></m-choose-time>
+    <m-choose-time @startChange="startChange" @endChange="endChange" :startOption="startOption" :endOption="endOption"></m-choose-time>
   </div>
 </template>
 
 <script lang='ts' setup>
 import {} from "vue"
+interface endValue {
+  startTime: string,
+  endTime: string
+}
+let startChange= (val: string)=> {
+  console.log('val: ', val);
+
+}
+let endChange= (val: endValue)=> {
+  console.log('val: ', val);
+}
+
+let startOption = {
+  size :'small',
+  clearable: false
+}
+
+let endOption = {
+
+}
 </script>
 
 <style lang="scss" scoped></style>
