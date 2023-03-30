57e18a040a3efce6e43e32bd56e18ce5bb72b5bb
DDang-L <1968806729@qq.com>
2023-03-30 16:45:04 +0800

    完善上传组件校验

--- src/components/form/src/index.vue
+++ src/components/form/src/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-form  v-if="model" v-bind="$attrs" :model="model" :rules="rules" :validate-on-rule-change="false">
+  <el-form ref="form"  v-if="model" v-bind="$attrs" :model="model" :rules="rules" :validate-on-rule-change="false">
     <template v-for="(item,index) in options" :key="index">
       <el-form-item v-if="!item.children || !item.children.length" :prop="item.prop" :label="item.label" >
         <component v-if="item.type !== 'upload'" :is="`el-${item.type}`" v-bind="item.attrs" v-model="model[item.prop!]"></component>
@@ -35,13 +35,21 @@
       </el-form-item>
 
     </template>
+    <el-form-item>
+      <!-- 作用域插槽：可以把实例分发出去 -->
+      <slot name="action" :form="form" :model="model"></slot>
+    </el-form-item>
+
   </el-form>
+
 </template>
 
 <script lang='ts' setup>
-import {PropType,ref,onMounted,watch} from "vue"
+import {PropType,ref,onMounted,watch,nextTick} from "vue"
 import { FormOptions } from "./type";
 import { cloneDeep } from "lodash";
+import type { ElForm } from 'element-plus';
+type FormInstance = InstanceType<typeof ElForm>;
 
 let emits = defineEmits(['on-preview','on-remove','on-success','on-error','on-progress','on-change','on-exceed','before-upload','before-remove'])
 
@@ -57,6 +65,7 @@ let props = defineProps({
 
 let model = ref<any>(null)
 let rules = ref<any>(null)
+let form = ref<FormInstance | null>(null)
 
 let initForm = () => {
   if(props.options && props.options.length) {
@@ -75,32 +84,35 @@ let initForm = () => {
 
 
 // 上传组件的所有方法
-let onPreview = (file:any) => {
+let onPreview = (file:File) => {
   emits('on-preview',file)
 }
-let onRemove = (file:any,fileList:any) => {
+let onRemove = (file:File,fileList:FileList) => {
   emits('on-remove',{ file,fileList})
 }
-let onSuccess = (response: any,file:any,fileList:any) => {
+let onSuccess = (response: any,file:File,fileList:FileList) => {
+  // 上传图片成功，给表单上传项赋值
+  let uploadItem = props.options?.find(item =>item.type === 'upload')
+  model.value[uploadItem?.prop!] = { response,file,fileList}
   console.log('success');
   emits('on-success',{ response,file,fileList})
 }
-let onError = (err:any,file:any,fileList:any) => {
+let onError = (err:any,file:File,fileList:FileList) => {
   emits('on-error',{ err,file,fileList})
 }
-let onProgress = (evt:any,file:any,fileList:any) => {
+let onProgress = (evt:any,file:File,fileList:FileList) => {
   emits('on-progress',{ evt,file,fileList})
 }
-let onChange = (file:any,fileList:any) => {
+let onChange = (file:File,fileList:FileList) => {
   emits('on-change',{ file,fileList})
 }
-let onExceed = (files:any,fileList:any) => {
+let onExceed = (files:any,fileList:FileList) => {
   emits('on-exceed',{ files,fileList})
 }
-let beforeUpload = (file:any) => {
+let beforeUpload = (file:File) => {
   emits('before-upload', file)
 }
-let beforeRemove = (file:any,fileList:any) => {
+let beforeRemove = (file:File,fileList:FileList) => {
   emits('before-remove',{ file,fileList})
 }
 
@@ -111,6 +123,20 @@ watch(()=>props.options,val => {
 onMounted(()=>{
   initForm()
 })
+
+// 重写表单重置方法
+const resetFields = () =>{
+  //加 nextTick=> 弹框初始化的时候 editorRef.value拿不到的bug
+  nextTick(() => {
+    form.value!.resetFields()
+  })
+}
+
+defineExpose({
+  resetFields
+})
+
+
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -1,16 +1,16 @@
 <template>
   <m-form 
-  label-width="100px" 
-  :options="options"
-  @on-preview="handlePreview"
-  @on-remove="handleRemove"
-  @before-remove="beforeRemove"
-  @on-exceed="handleExceed"
-  @on-success="handleSuccess"
-  @on-change="handleChange"
-  @before-upload="handleBeforeUpload"
-  >
-     <template #uploadArea>
+    ref="formRef" 
+    label-width="100px" 
+    :options="options" 
+    @on-preview="handlePreview" 
+    @on-remove="handleRemove"
+    @before-remove="beforeRemove" 
+    @on-exceed="handleExceed" 
+    @on-success="handleSuccess" 
+    @on-change="handleChange"
+    @before-upload="handleBeforeUpload">
+    <template #uploadArea>
       <el-button type="primary">点击</el-button>
     </template>
     <template #uploadTip>
@@ -18,197 +18,226 @@
         jpg/png files with a size less than 500KB.
       </div>
     </template>
+
+    <template #action="scope">
+      <el-button type="primary" @click="submitForm(scope)">提交</el-button>
+      <el-button @click="resetForm(scope)">重置</el-button>
+    </template>
   </m-form>
 </template>
 
 <script lang='ts' setup>
-import {} from "vue"
+import {ref} from 'vue'
 import { FormOptions } from "../../components/form/src/type";
 import { ElMessage, ElMessageBox } from 'element-plus'
+import type { ElForm } from 'element-plus';
+type FormInstance = InstanceType<typeof ElForm>;
 
-let options:FormOptions [] =[
+  interface Scope {
+  form: FormInstance,
+  model: any,
+}
+let options: FormOptions[] = [
   {
-    type:'input',
-    value:'',
-    label:'用户名',
-    prop:'username',
-    rules:[
+    type: 'input',
+    value: '',
+    label: '用户名',
+    prop: 'username',
+    rules: [
       {
         required: true,
-        message:'用户名不能为空',
-        trigger:'blur'
+        message: '用户名不能为空',
+        trigger: 'blur'
       },
       {
         min: 2,
-        max:6,
-        message:'用户名在2-6位之间',
-        trigger:'blur'
+        max: 6,
+        message: '用户名在2-6位之间',
+        trigger: 'blur'
       },
     ],
-    attrs:{
-      clearable:true
+    attrs: {
+      clearable: true
     }
   },
   {
-    type:'input',
-    value:'',
-    label:'密码',
-    prop:'password',
-    rules:[
+    type: 'input',
+    value: '',
+    label: '密码',
+    prop: 'password',
+    rules: [
       {
         required: true,
-        message:'密码不能为空',
-        trigger:'blur'
+        message: '密码不能为空',
+        trigger: 'blur'
       },
       {
         min: 6,
-        max:16,
-        message:'密码在6-15位之间',
-        trigger:'blur'
+        max: 16,
+        message: '密码在6-15位之间',
+        trigger: 'blur'
       },
     ],
-    attrs:{
+    attrs: {
       showPassword: true,
-      clearable:true
+      clearable: true
     }
   },
   {
-    type:'select',
-    value:'',
-    placholder:'请选择职位',
-    prop:'role',
-    label:'职位',
-    attrs:{
-      style:{
-        width:'100%'
+    type: 'select',
+    value: '',
+    placholder: '请选择职位',
+    prop: 'role',
+    label: '职位',
+    attrs: {
+      style: {
+        width: '100%'
       }
     },
-    rules:[
+    rules: [
       {
         required: true,
-        message:'职位不能为空',
-        trigger:'blur',
+        message: '职位不能为空',
+        trigger: 'blur',
       }
     ],
-    children:[
+    children: [
       {
-        type:'option',
-        label:'经理',
+        type: 'option',
+        label: '经理',
         value: '1',
       },
       {
-        type:'option',
-        label:'主管',
+        type: 'option',
+        label: '主管',
         value: '2',
       },
       {
-        type:'option',
-        label:'员工',
+        type: 'option',
+        label: '员工',
         value: '3',
       },
     ],
-    
+
   },
   {
-    type:'checkbox-group',
+    type: 'checkbox-group',
     value: [],
-    prop:'like',
-    label:'爱好',
-    rules:[
+    prop: 'like',
+    label: '爱好',
+    rules: [
       {
         required: true,
-        message:'爱好不能为空',
-        trigger:'blur',
+        message: '爱好不能为空',
+        trigger: 'blur',
       }
     ],
-    children:[
+    children: [
       {
-        type:'checkbox',
-        label:'足球',
-        value:'1'
+        type: 'checkbox',
+        label: '足球',
+        value: '1'
       },
       {
-        type:'checkbox',
-        label:'篮球',
-        value:'2'
+        type: 'checkbox',
+        label: '篮球',
+        value: '2'
       },
       {
-        type:'checkbox',
-        label:'排球',
-        value:'3'
+        type: 'checkbox',
+        label: '排球',
+        value: '3'
       },
     ]
   },
   {
-    type:'radio-group',
+    type: 'radio-group',
     value: 'male',
-    prop:'gender',
-    label:'性别',
-    rules:[
+    prop: 'gender',
+    label: '性别',
+    rules: [
       {
         required: true,
-        message:'性别不能为空',
-        trigger:'blur',
+        message: '性别不能为空',
+        trigger: 'blur',
       }
     ],
-    children:[
+    children: [
       {
-        type:'radio',
-        label:'男',
-        value:'male'
+        type: 'radio',
+        label: '男',
+        value: 'male'
       },
       {
-        type:'radio',
-        label:'女',
-        value:'female'
+        type: 'radio',
+        label: '女',
+        value: 'female'
       },
       {
-        type:'radio',
-        label:'保密',
-        value:'not'
+        type: 'radio',
+        label: '保密',
+        value: 'not'
       },
     ]
   },
   {
-    type:'upload',
-    label:'上传',
-    prop:'pic',
-    rules:[
+    type: 'upload',
+    label: '上传',
+    prop: 'pic',
+    rules: [
       {
         required: true,
-        message:'不能为空',
-        trigger:'blur',
+        message: '图片不能为空',
+        trigger: 'blur',
       }
     ],
-    uploadAttrs:{
-      action:'https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15',
+    uploadAttrs: {
+      action: "https://jsonplaceholder.typicode.com/posts/",
       multiple: true,
-      limit:3,
+      limit: 3,
     }
   }
 ]
 
-const handleRemove = (val:any) => {
-  let {file, fileList} = val
-  console.log('handleRemove',file, fileList)
+let formRef = ref<any>(null)
+
+let submitForm = (data: Scope) => {
+  data.form.validate((valid) => {
+    if(valid) {
+      console.log(data.model);
+      ElMessage.success('提交成功');
+    }else {
+      ElMessage.error('请填写表单');
+    }
+  })
+}
+let resetForm = (data: Scope) => {
+  // data.form.removeField()
+
+   // 调用封装form的重置方法
+   formRef.value.resetFields()
 }
 
-const handlePreview = (val:any) => {
-  let {file, fileList} = val
-  console.log('handlePreview',file, fileList)
+const handleRemove = (val: any) => {
+  let { file, fileList } = val
+  console.log('handleRemove', file, fileList)
 }
 
-const handleExceed = (val:any) => {
-  let {files, fileList} = val
+const handlePreview = (val: any) => {
+  let { file, fileList } = val
+  console.log('handlePreview', file, fileList)
+}
+
+const handleExceed = (val: any) => {
+  let { files, fileList } = val
   ElMessage.warning(
-    `handleExceed The limit is 3, you selected ${files.length} files this time, add up to ${
-      files.length + fileList.length
+    `handleExceed The limit is 3, you selected ${files.length} files this time, add up to ${files.length + fileList.length
     } totally`
   )
 }
 
-const beforeRemove = (val:any) => {
-  let {file, fileList} = val
+const beforeRemove = (val: any) => {
+  let { file, fileList } = val
   return ElMessageBox.confirm(
     `beforeRemove Cancel the transfert of ${file.name} ?`
   ).then(
@@ -217,19 +246,19 @@ const beforeRemove = (val:any) => {
   )
 }
 
-let handleSuccess = (val:any) =>{
-  let {response,file, fileList} = val
+let handleSuccess = (val: any) => {
+  let { response, file, fileList } = val
   console.log('val: ', val);
-  console.log('handleSuccess',response,file,fileList);
+  console.log('handleSuccess', response, file, fileList);
 }
 
-let handleChange = (val:any)=>{
-  let {file, fileList} = val
-  console.log('handleChange',file,fileList);
+let handleChange = (val: any) => {
+  let { file, fileList } = val
+  console.log('handleChange', file, fileList);
 }
 
-let handleBeforeUpload = (val:any) =>{
-  console.log('handleBeforeUpload',val);
+let handleBeforeUpload = (val: any) => {
+  console.log('handleBeforeUpload', val);
 }
 </script>
 
